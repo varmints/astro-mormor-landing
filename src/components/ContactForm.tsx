ac8707aa619a3690cd91c2381a7ab7308195b2f6
Kamil <example@example.com>
2023-01-08 21:31:32 +0100

    change spam protection to turnstile

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, FormEvent, useRef, useState } from "react";
-import ReCAPTCHA from "react-google-recaptcha";
+import Turnstile from "react-turnstile";
 
 type FormState = {
   email: string;
@@ -14,8 +14,7 @@ type ServiceMessage = {
 const ContactForm = () => {
   const formId = "zjyGPPkG";
   const formSparkUrl = `https://submit-form.com/${formId}`;
-  const recaptchaKey = "6LeWaNkjAAAAAHKdyBJa1QmkdmuTDRtteFMo4sHu";
-  const recaptchaRef = useRef<any>();
+  const captchaKey = "1x00000000000000000000AA";
 
   const initialFormState = {
     email: "",
@@ -25,7 +24,7 @@ const ContactForm = () => {
   const [formState, setFormState] = useState<FormState>(initialFormState);
   const [submitting, setSubmitting] = useState<boolean>(false);
   const [message, setMessage] = useState<ServiceMessage>();
-  const [recaptchaToken, setReCaptchaToken] = useState<string>();
+  const [captchaToken, setCaptchaToken] = useState<string>();
 
   const submitForm = async (event: FormEvent) => {
     event.preventDefault();
@@ -37,7 +36,7 @@ const ContactForm = () => {
   const postSubmission = async () => {
     const payload = {
       ...formState,
-      "g-recaptcha-response": recaptchaToken,
+      "cf-turnstile-response": captchaToken,
     };
 
     try {
@@ -50,16 +49,18 @@ const ContactForm = () => {
         body: JSON.stringify(payload),
       });
       console.log(result);
-      setMessage({
-        class: "bg-green-500",
-        text: "Dzięki, wkrótce się skontaktuję.",
-      });
-      setFormState(initialFormState);
-      recaptchaRef.current.reset();
+      if (result.ok) {
+        setMessage({
+          class: "bg-green-700",
+          text: "Dzięki, wkrótce się skontaktuję.",
+        });
+        setFormState(initialFormState);
+      }
+      throw new Error("Something went wrong.");
     } catch (error) {
       console.log(error);
       setMessage({
-        class: "bg-red-500",
+        class: "bg-red-700",
         text: "Przepraszam, wystąpił problem. Spróbuj ponownie lub skontaktuj się za pośrednictwem mediów społecznościwych.",
       });
     }
@@ -75,8 +76,8 @@ const ContactForm = () => {
     setFormState(updatedFormState);
   };
 
-  const updateRecaptchaToken = (token: string | null) => {
-    setReCaptchaToken(token as string);
+  const updateCaptchaToken = (token: string | null) => {
+    setCaptchaToken(token as string);
   };
 
   return (
@@ -96,7 +97,8 @@ const ContactForm = () => {
               value={formState?.email}
               onChange={updateFormControl}
               className="peer h-10 w-full border-b-2 border-current bg-transparent placeholder-transparent text-sm focus:outline-none"
-              placeholder="Ades email"
+              placeholder="Adres email"
+              required
             />
             <label
               htmlFor="email"
@@ -115,6 +117,7 @@ const ContactForm = () => {
               rows={8}
               minLength={8}
               placeholder="Treść wiadomości"
+              required
             ></textarea>
             <label
               htmlFor="textarea"
@@ -123,12 +126,14 @@ const ContactForm = () => {
               Treść wiadomości
             </label>
           </div>
-          <div className="relative">
-            <ReCAPTCHA
-              ref={recaptchaRef}
-              sitekey={recaptchaKey}
-              onChange={updateRecaptchaToken}
+          <div>
+            <Turnstile
+              sitekey={captchaKey}
+              onVerify={updateCaptchaToken}
+              theme="dark"
             />
+          </div>
+          <div>
             <button
               disabled={submitting}
               className="flex items-center justify-center gap-3 border-2 border-current px-6 py-4 hover:text-secondary focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2 focus:ring-offset-transparent"
